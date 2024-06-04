a634c577b73ab9a86f1b1451391c67fb92f1cd05
Daniil Strelnikov <stredaniil@gmail.com>
2024-06-04 17:28:52 +0300

    add calendar flow

--- components/components/Calendar/index.tsx
+++ components/components/Calendar/index.tsx
@@ -1,12 +1,84 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Wrapper } from "../Wrapper";
 import { TouchableOpacity, View } from "react-native";
 import { Text } from "../Text";
 import style from "./style";
 import { Space } from "../Space";
 import { PressableIcon } from "../PressableIcon";
+import moment from "moment";
+
+const months = [
+	"январь",
+	"февраль",
+	"март",
+	"апрель",
+	"май",
+	"июнь",
+	"июль",
+	"август",
+	"сентябрь",
+	"октябрь",
+	"ноябрь",
+	"декабрь",
+];
+
+type Date = {
+	year: number;
+	month: number;
+	day?: number;
+};
+
+type DayProps = {
+	date: Date;
+	calendar: Date;
+	id: number;
+	pickDate: (id: number) => void;
+};
 
 export const Calendar = () => {
+	const momentRef = useRef(moment());
+
+	const [calendar, setCalendar] = useState<Date>({
+		year: moment().year(),
+		month: moment().month(),
+	});
+	const [pickedDay, setPickedDay] = useState<Date>({
+		year: moment().year(),
+		month: moment().month(),
+		day: moment().day() + 1,
+	});
+
+	const days = useMemo(
+		() => [
+			...new Array(
+				moment()
+					.set({ ...calendar })
+					.daysInMonth()
+			),
+		],
+		[calendar]
+	);
+
+	const handleAddMonth = () => {
+		const date = momentRef.current.add(1, "M");
+		setCalendar({
+			year: date.year(),
+			month: date.month(),
+		});
+	};
+
+	const handleSubtractMonth = () => {
+		const date = momentRef.current.subtract(1, "M");
+		setCalendar({
+			year: date.year(),
+			month: date.month(),
+		});
+	};
+
+	const handlePickDay = (id: number) => {
+		setPickedDay({ ...calendar, day: id });
+	};
+
 	return (
 		<Wrapper>
 			<View style={style.monthWrapper}>
@@ -14,30 +86,54 @@ export const Calendar = () => {
 					name="arrow-left"
 					size={36}
 					color="black"
-					onPress={() => console.log("hello world!")}
+					onPress={handleSubtractMonth}
 				/>
 				<Text size={18} weight="bold">
-					Январь
+					{`${months[calendar.month].toUpperCase()} - ${calendar.year}`}
 				</Text>
 				<PressableIcon
 					name="arrow-right"
 					size={36}
 					color="black"
-					onPress={() => console.log("hello world!")}
+					onPress={handleAddMonth}
 				/>
 			</View>
 
 			<Space v={15} />
 
 			<View style={style.daysWrapper}>
-				{[...new Array(30)].map((el, id) => (
-					<TouchableOpacity style={[style.date]} key={"date " + id}>
-						<Text size={16} weight="bold">
-							{id + 1}
-						</Text>
-					</TouchableOpacity>
+				{days.map((_, id) => (
+					<Day
+						calendar={calendar}
+						date={pickedDay}
+						id={id}
+						pickDate={handlePickDay}
+						key={"day-" + id}
+					/>
 				))}
 			</View>
 		</Wrapper>
+	);
+};
+
+const Day = ({ date, calendar, id, pickDate }: DayProps) => {
+	const compareDate = () =>
+		date.month === calendar.month &&
+		date.year === calendar.year &&
+		date.day == id;
+
+	const SelectedWrapperStyle = compareDate() && style.activeDate;
+	const SelectedTextStyle = compareDate() && (style.activeDate as any);
+
+	return (
+		<TouchableOpacity
+			style={[style.date, SelectedWrapperStyle]}
+			key={"date " + id}
+			onPress={() => pickDate(id)}
+		>
+			<Text size={16} weight="bold" style={SelectedTextStyle}>
+				{id + 1}
+			</Text>
+		</TouchableOpacity>
 	);
 };
